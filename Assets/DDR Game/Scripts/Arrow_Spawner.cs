aa28db6c57541f44fe158008683a76a54df922c0
Greg <gbretall@uci.edu>
2016-01-27 15:42:42 -0800

    Removed a debug print statement from Arrow_Spawner

--- Assets/DDR Game/Scripts/Arrow_Spawner.cs
+++ Assets/DDR Game/Scripts/Arrow_Spawner.cs
@@ -76,7 +76,6 @@ public class Arrow_Spawner : MonoBehaviour {
             {
                 if (am.spawnTime == Time.time)
                 {
-                    Debug.Log("Hello World");
                     am.GetComponentInParent<SpriteRenderer>().enabled = true;
                     am.moving = true;
                 }
